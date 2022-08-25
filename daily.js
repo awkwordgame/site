fd796a8dbe4727ca98277f557be75c304817ce68
awkwordgame <awkwordgame@gmail.com>
2022-08-25 16:21:59 +0000

    update

--- daily.js
+++ daily.js
@@ -4,10 +4,12 @@ localStorage.setObj("daily", [
     "8aaaaaapbbx",
     "8aaaaaapbbx",
     "8aaaaaapbbx",
-    "0020211226324552677289281290204253123mousetrap",
-    "0020211228324582678289201220204253123house",
-    "0020211228324582678289201220204253123cook",
     "9979788771675417321710798779795746876pbbx",
+    "8aaaaaapbbx",
+    "8aaaaaapbbx",
+    "8aaaaaapbbx",
+    "8aaaaaapbbx",
+    "8aaaaaapbbx",
     "8aaaaaapbbx",
   ]
 );
